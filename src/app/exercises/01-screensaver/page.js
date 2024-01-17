378012951b23e13f6a7ee19c5a8e60cff1fb6f0c
Katarina-Slotova <katarina.slotova@gmail.com>
2024-01-17 16:23:54 -0500

    Complete screensaver routing exercise.

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from 'react'
 
-import ScreenSaver from '../../../components/ScreenSaver';
+import Link from 'next/link'
 
-function ScreenSaverExercise() {
+function ScreenSaverIndexPage() {
   return (
-    <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
+    <main>
+      <p>Choose your color:</p>
+      <ul>
+        <li>
+          <Link href='/exercises/01-screensaver/slateblue'>slateblue</Link>
+        </li>
+        <li>
+          <Link href='/exercises/01-screensaver/hotpink'>hotpink</Link>
+        </li>
+        <li>
+          <Link href='/exercises/01-screensaver/peachpuff'>peachpuff</Link>
+        </li>
+        <li>
+          <Link href='/exercises/01-screensaver/white'>white</Link>
+        </li>
+      </ul>
     </main>
-  );
+  )
 }
 
-export default ScreenSaverExercise;
+export default ScreenSaverIndexPage
